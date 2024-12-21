238b4ac926e3e26603c5ddebc4b8b46cd8718cf9
Sazi Ndlovu <nerdimee@gmail.com>
2024-12-21 17:58:43 +0200

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
-for i in range(1, 10):  # Looping through numbers 1 to 10
+for i in range(1, 11):  # Looping through numbers 1 to 10
     product = number * i
     print(f"{number} * {i} = {product}")
